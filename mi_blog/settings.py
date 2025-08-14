1588643c07e604c13e7bf965f5f90c0484a962c4
Rodrigo Camps <camps_cat@hotmail.com>
2025-08-13 21:46:10 -0300

    Primer commit2

--- mi_blog/settings.py
+++ mi_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pqid=^yavt0h^-!mfg7opz==6km-apue*ll6&)#-g7vo)9mwpv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
